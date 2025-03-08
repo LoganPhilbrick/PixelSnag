007bf91b5e431a53de26ab1a829e54eb4d514877
Trevor <trevorphilbrick@outlook.com>
2025-03-07 18:31:17 -0800

    updated favicons

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,6 +17,26 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+<<<<<<< Updated upstream
+=======
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+>>>>>>> Stashed changes
       <body>
         <SupabaseProvider supabaseUrl={supabaseUrl} supabaseKey={supabaseKey}>
           <div id="root">{children}</div>

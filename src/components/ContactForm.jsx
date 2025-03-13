fadf76fc60b5a880cceaf43d1df8a8b7646a2b1e
LoganPhilbrick <loganphilbrick99@gmail.com>
2025-03-13 17:40:09 -0400

    changed styles for bigger screens

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -45,13 +45,13 @@ function ContactForm() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center mt-12 sm:mt-24">
-      <h2 className="text-2xl md:text-3xl lg:text-5xl px-6 font-semibold bg-gradient-to-b from-white to-zinc-400 text-transparent bg-clip-text p-1.5 mb-12 sm:mb-24">Wanna get in touch?</h2>
-      <div className="container md:w-1/2 w-full h-full px-6 pb-6 mx-auto mb-24 rounded-xl sm:bg-neutral-900">
-        <h1 className="text-2xl font-bold sm:my-4 pl-7 sm:pl-0 text-neutral-200">Feedback</h1>
+    <div className="flex flex-col items-center mt-12 sm:mt-24 sm:px-0 px-8">
+      <h2 className="text-2xl md:text-3xl lg:text-5xl px-6 font-semibold bg-gradient-to-b from-white to-zinc-400 text-transparent bg-clip-text mb-12 py-1.5 sm:mb-24">Wanna get in touch?</h2>
+      <div className="container sm:w-5/6 md:w-4/6 lg:w-2/3 xl:w-2/5 w-full h-full px-6 pb-6 mx-auto mb-24 rounded-xl bg-neutral-900">
+        <h1 className="text-2xl font-bold sm:my-4 py-4 text-neutral-200">Feedback</h1>
         <div className=" flex items-center justify-center ">
           <iframe
-            className="rounded-lg sm:bg-neutral-800 p-6"
+            className="rounded-lg bg-neutral-800 p-6"
             data-tally-src="https://tally.so/embed/nPk2px?alignLeft=1&hideTitle=1&dynamicHeight=1"
             loading="lazy"
             width="100%"

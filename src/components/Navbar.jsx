415f26cfbd174429876e600a07309803ef0824f5
Trevor <trevorphilbrick@outlook.com>
2025-03-15 16:09:17 -0700

    added a loading state to the log in screen and added a menu for small screens home landing page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,99 @@
+"use client";
 import Link from "next/link";
-import { createClient } from "../utils/supabase/server";
+import { createClient } from "../utils/supabase/client";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
-export default async function Navbar() {
-  const supabase = await createClient();
-  const { data } = await supabase.auth.getUser();
+export default function Navbar() {
+  const supabase = createClient();
+  const [isOpen, setIsOpen] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await supabase.auth.getUser();
+      setData(data);
+    };
+    fetchData();
+  }, [supabase]);
 
   return (
-    <nav className="flex items-center justify-between w-3/4 h-48">
+    <nav className="flex items-center justify-between h-48 w-full px-4">
       <div className="flex items-center gap-2">
         <Image src="/logo.svg" alt="logo" width={24} height={24} />
         <h2 className="text-3xl">PixelSnag</h2>
       </div>
-      <div className="md:inline hidden">
-        {!data.user && (
-          <>
+      <div className="inline md:hidden">
+        <button onClick={() => setIsOpen(!isOpen)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 text-neutral-200"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+      </div>
+      {isOpen && (
+        <div className="absolute top-0 left-0 w-full h-screen bg-neutral-900/50 backdrop-blur-sm">
+          <button onClick={() => setIsOpen(false)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 absolute top-20 right-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+          <div className="flex flex-col p-4 justify-center h-full gap-4">
             <Link
               href="/login"
-              className="bg-black py-4 px-8 mr-4 rounded-full hover:bg-neutral-900 transition-all duration-200"
+              className="bg-black py-4 px-8 rounded-full hover:bg-neutral-900 transition-all duration-200 text-center"
             >
               Login
             </Link>
             <Link
               href="/signup"
-              className="bg-white text-black py-4 px-8 rounded-full hover:bg-neutral-200 transition-all duration-200"
+              className="bg-white text-black py-4 px-8 rounded-full hover:bg-neutral-200 transition-all duration-200 text-center"
             >
               Signup
             </Link>
-          </>
-        )}
-        {data.user && (
+          </div>
+        </div>
+      )}
+      <div className="md:inline hidden">
+        {!data ||
+          (!data.user && (
+            <>
+              <Link
+                href="/login"
+                className="bg-black py-4 px-8 mr-4 rounded-full hover:bg-neutral-900 transition-all duration-200"
+              >
+                Login
+              </Link>
+              <Link
+                href="/signup"
+                className="bg-white text-black py-4 px-8 rounded-full hover:bg-neutral-200 transition-all duration-200"
+              >
+                Signup
+              </Link>
+            </>
+          ))}
+        {data && data.user && (
           <Link
             href="/dashboard"
             className="bg-black py-4 px-8 mr-4 rounded-full hover:bg-neutral-900 transition-all duration-200"
@@ -38,7 +102,6 @@ export default async function Navbar() {
           </Link>
         )}
       </div>
-      {/* eslint-disable-next-line no-undef */}
     </nav>
   );
 }

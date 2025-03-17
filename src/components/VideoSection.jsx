8630fce9bd0bed83d06b7f04571bcd95c9550a8b
LoganPhilbrick <loganphilbrick99@gmail.com>
2025-03-17 18:52:15 -0400

    condensed styling and custom text for each video

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -3,21 +3,52 @@ import React from "react";
 export default function VideoSection() {
   return (
     <div className="flex flex-col items-center w-full">
-      <h2 className="text-xl md:text-3xl lg:text-5xl px-6 font-semibold bg-gradient-to-b from-white to-zinc-400 text-transparent bg-clip-text mt-12 sm:mt-24 p-1.5">Easy as 1, 2, 3...</h2>
-      <div className="flex lg:flex-row flex-col-reverse justify-evenly xl:justify-between items-center lg:w-full xl:w-2/3 mt-12 sm:mt-24">
+      <h2 className="text-xl md:text-3xl lg:text-5xl px-6 font-semibold bg-gradient-to-b from-white to-zinc-400 text-transparent bg-clip-text my-12 sm:my-24 p-1.5">Easy as 1, 2, 3...</h2>
+      <div className="flex xl:flex-row flex-col-reverse items-center justify-between md:w-2/3 xl:w-2/3 xl:px-0 px-12 mb-24">
+        <video autoPlay loop muted className="w-full xl:w-1/2 rounded-xl">
+          <source src="https://cgkethnnnnsxbzvglfmn.supabase.co/storage/v1/object/public/sample-videos//ss.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="flex items-start w-full xl:w-fit xl:px-24 mb-12 xl:mb-0">
+          <p className="text-5xl font-semibold mr-4">1</p>
+          <p className="text-sm sm:text-base text-start">Take a screenshot by pressing CTRL(CMD on Mac) + ALT + SHFT + S on your keyboard.</p>
+        </div>
+      </div>
+      <div className="flex xl:flex-row flex-col items-center justify-between md:w-2/3 xl:w-2/3 xl:px-0 px-12 mb-24">
+        <div className="flex items-start w-full xl:w-fit xl:px-24 mb-12 xl:mb-0">
+          <p className="text-5xl font-semibold mr-4">2</p>
+          <p className="text-sm sm:text-base text-start">Screenshots will automatically open in the PixelSnag photo editor.</p>
+        </div>
+        <video autoPlay loop muted className="w-full xl:w-1/2 rounded-xl">
+          <source src="https://cgkethnnnnsxbzvglfmn.supabase.co/storage/v1/object/public/sample-videos//edit.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="flex xl:flex-row flex-col-reverse items-center justify-between md:w-2/3 xl:w-2/3 xl:px-0 px-12">
+        <video autoPlay loop muted className="w-full xl:w-1/2 rounded-xl">
+          <source src="https://cgkethnnnnsxbzvglfmn.supabase.co/storage/v1/object/public/sample-videos//share.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="flex items-start w-full xl:w-fit xl:px-24 mb-12 xl:mb-0">
+          <p className="text-5xl font-semibold mr-4">3</p>
+          <p className="text-sm sm:text-base text-start">Edited screenshots can be exported or copied directly to the clipboard for pasting and sharing.</p>
+        </div>
+      </div>
+
+      {/* <div className="flex lg:flex-row flex-col-reverse justify-evenly xl:justify-between items-center lg:px-12 lg:w-full xl:w-2/3 mt-12 sm:mt-24 bg-sky-500">
         <video autoPlay loop muted className="w-5/6 md:w-2/3 lg:w-1/2 rounded-xl">
           <source src="https://cgkethnnnnsxbzvglfmn.supabase.co/storage/v1/object/public/sample-videos//ss.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div className="flex items-center justify-center xl:p-24">
+        <div className="flex items-center justify-center lg:pl-24 lg:mb-0 mb-12">
           <p className="text-5xl font-semibold mr-4">1</p>
-          <p className="w-5/6 md:w-2/3 lg:w-1/3 xl:w-full text-start lg:mb-0 mb-12">Take a screenshot by pressing CTRL + ALT + SHFT + S on your keyboard.</p>
+          <p className="w-fit text-start">Take a screenshot by pressing CTRL + ALT + SHFT + S on your keyboard.</p>
         </div>
-      </div>
-      <div className="flex lg:flex-row flex-col justify-evenly xl:justify-between items-center lg:w-full xl:w-2/3 mt-12 sm:mt-24">
-        <div className="flex items-center justify-center xl:p-24">
+      </div> */}
+      {/* <div className="flex lg:flex-row flex-col justify-evenly xl:justify-between items-center lg:w-full xl:w-2/3 mt-12 sm:mt-24">
+        <div className="flex items-center justify-center xl:pr-24 lg:mb-0 mb-12">
           <p className="text-5xl font-semibold mr-4">2</p>
-          <p className="w-5/6 md:w-2/3 lg:w-1/3 xl:w-full text-start lg:mb-0 mb-12">Take a screenshot by pressing CTRL + ALT + SHFT + S on your keyboard.</p>
+          <p className="w-5/6 md:w-2/3 lg:w-1/3 xl:w-full text-start">Take a screenshot by pressing CTRL + ALT + SHFT + S on your keyboard.</p>
         </div>
         <video autoPlay loop muted className="w-5/6 md:w-2/3 lg:w-1/2 rounded-xl">
           <source src="https://cgkethnnnnsxbzvglfmn.supabase.co/storage/v1/object/public/sample-videos//edit.mp4" />
@@ -29,11 +60,11 @@ export default function VideoSection() {
           <source src="https://cgkethnnnnsxbzvglfmn.supabase.co/storage/v1/object/public/sample-videos//share.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div className="flex items-center justify-center xl:p-24">
+        <div className="flex items-center justify-center xl:pl-24 lg:mb-0 mb-12">
           <p className="text-5xl font-semibold mr-4">3</p>
-          <p className="w-5/6 md:w-2/3 lg:w-1/3 xl:w-full text-start lg:mb-0 mb-12">Take a screenshot by pressing CTRL + ALT + SHFT + S on your keyboard.</p>
-        </div>
-      </div>
+          <p className="w-5/6 md:w-2/3 lg:w-1/3 xl:w-full text-start">Take a screenshot by pressing CTRL + ALT + SHFT + S on your keyboard.</p>
+        </div> */}
+      {/* </div> */}
     </div>
   );
 }

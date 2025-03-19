2d64d4c51d8e8a8782cc77ec7eba62caa64daffc
Trevor <trevorphilbrick@outlook.com>
2025-03-18 21:18:22 -0700

    removed cors headers

--- next.config.mjs
+++ next.config.mjs
@@ -7,25 +7,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        // Allow CORS for all routes
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,PUT,DELETE,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;

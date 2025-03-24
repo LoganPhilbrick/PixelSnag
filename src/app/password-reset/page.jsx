e31206603c0ec4015cf6bede9ca81cfa8d1306fa
Trevor <trevorphilbrick@outlook.com>
2025-03-23 21:18:26 -0700

    updated navigation import

--- src/app/password-reset/page.jsx
+++ src/app/password-reset/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { createClient } from "../../utils/supabase/client";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
   const [isLoading, setIsLoading] = useState(false);

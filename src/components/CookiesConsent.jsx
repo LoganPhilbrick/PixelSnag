1f5dc598076a51f5bbfb9dfbd95051dc145cf5b8
Trevor <trevorphilbrick@outlook.com>
2025-04-10 18:53:44 -0700

    removed cookies consent lockout

--- src/components/CookiesConsent.jsx
+++ src/components/CookiesConsent.jsx
@@ -19,20 +19,17 @@ const CookieConsent = () => {
   }
 
   return (
-    <div className="fixed inset-0 bg-slate-700 bg-opacity-70 z-50">
-      <div className="fixed bottom-0 left-0 right-0 flex items-center justify-between p-4 bg-neutral-900 z-50">
-        <span className="text-dark text-base mr-16">
-          This website uses cookies to improve user experience. By using our
-          website you consent to all cookies in accordance with our Cookie
-          Policy.
-        </span>
-        <button
-          className="bg-blue-600 hover:bg-blue-700 py-2 px-8 rounded text-white"
-          onClick={() => acceptCookie()}
-        >
-          Accept
-        </button>
-      </div>
+    <div className="fixed bottom-0 left-0 right-0 flex items-center justify-between p-4 bg-neutral-900 z-50">
+      <span className="text-dark text-base mr-16">
+        This website uses cookies to improve user experience. By using our
+        website you consent to all cookies in accordance with our Cookie Policy.
+      </span>
+      <button
+        className="bg-blue-600 hover:bg-blue-700 py-2 px-8 rounded text-white"
+        onClick={() => acceptCookie()}
+      >
+        Accept
+      </button>
     </div>
   );
 };
